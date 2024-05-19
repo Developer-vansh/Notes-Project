4bc4a320da74a0970ee9580560425e29114dba06
Nikhil <117506262+Nikhil-Sarwara@users.noreply.github.com>
2024-05-19 23:16:31 +0530

    add registration details

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Register.css";
 
 function Register() {
-  return <div>Register Page</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "http://localhost:8002/api/v1/user/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("User created successfully:", data);
+
+        // Make alert or redirect to another page
+        alert("User created successfully");
+
+        // Reset the form
+        setFormData({
+          name: "",
+          email: "",
+          password: "",
+        });
+
+        // redirect to login page
+        window.location.href = "/login";
+      } else {
+        console.error("Failed to create user");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="register-button">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
